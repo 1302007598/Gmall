0a4af36affccc23e1c826315af3e4e18c0d6d58a
hanxiao <1302007598@qq.com>
2021-01-29 20:40:23 +0800

    gmall-realtime-spark

---
+++ gmall-realtime-spark/src/main/scala/com/hanxiao/gmall/realtime/bean/DauInfo.scala
@@ -0,0 +1,14 @@
+package com.hanxiao.gmall.realtime.bean
+
+case class DauInfo(
+                      mid:String,
+                      uid:String,
+                      ar:String,
+                      ch:String,
+                      vc:String,
+                      var dt:String,
+                      var hr:String,
+                      ts:Long) {
+  }
+
+
